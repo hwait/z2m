cdc4286b2dc3db85ce44a4fb2d3f702c393c7bbf
hwait <hwaitt@gmail.com>
2024-09-15 20:18:11 +0200

    Subscriptions endpoint, db 1st

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
 }
+
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+    // HttpResponse::Ok().body(format!("{} subscribed!", _form.name))
+}
+
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
@@ -13,9 +20,8 @@ async fn health_check() -> impl Responder {
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
