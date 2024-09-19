c2193c127ed818c80a2dfc82e067caa400d95e4d
Nikolai Kashavkin <hwaitt@gmail.com>
2024-09-19 20:57:08 +0200

    Merge pull request #2
    * Persisting A New Subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::net::TcpListener;
-use z2m::startup::run;
 use z2m::configuration::get_configuration;
+use z2m::startup::run;
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    let connection_pool = PgPool::connect(
+        &configuration.database.connection_string()
+    )
+        .await
+        .expect("Failed to connect to database");
+
+    run(listener, connection_pool)?.await
 }
