a297fbbe1fc9d1e60f33a517693fbf01492f8b29
hwait <hwaitt@gmail.com>
2024-09-20 21:23:07 +0200

    Implementation logging

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
 use std::net::TcpListener;
 use z2m::configuration::get_configuration;
 use z2m::startup::run;
-use sqlx::PgPool;
+use z2m::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("z2m".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    let connection_pool = PgPool::connect(
-        &configuration.database.connection_string()
-    )
-        .await
-        .expect("Failed to connect to database");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to database");
 
     run(listener, connection_pool)?.await
 }
