5253526abc312b39c5995d9c58892e2607e53500
hwait <hwaitt@gmail.com>
2024-09-23 20:22:27 +0200

    Form Name validation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -79,28 +79,38 @@ async fn test_health_check() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_for_valid_form_data() {
-    let test_app = spawn_app().await;
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
-    let body = "name=hwait&email=hwaitt@gmail.com";
-    let response = client
-        .post(&format!("{}/subscriptions", &test_app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
 
-    assert_eq!(200, response.status().as_u16());
-
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
-        .fetch_one(&test_app.db_pool)
-        .await
-        .expect("Failed to fetch saved subscription.");
-
-    assert_eq!(saved.email, "hwaitt@gmail.com");
-    assert_eq!(saved.name, "hwait");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            description
+        );
+    }
+    // let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+    //     .fetch_one(&test_app.db_pool)
+    //     .await
+    //     .expect("Failed to fetch saved subscription.");
+    //
+    // assert_eq!(saved.email, "hwaitt@gmail.com");
+    // assert_eq!(saved.name, "hwait");
 }
 
 #[tokio::test]
