539e9b67c50559cd9dca37dd4a16e0feebe27bae
hwait <hwaitt@gmail.com>
2024-09-24 21:44:42 +0200

    Form Name validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -12,27 +12,34 @@ pub struct FormData {
 
 #[tracing::instrument(
     name = "Adding a new subscription",
-skip(form, pool),
-fields(
-subscriber_email=%form.email,
-subscriber_name=%form.name
-)
+    skip(form, pool),
+    fields(
+        subscriber_email=%form.email,
+        subscriber_name=%form.name
+    )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    // It is possible to use form.0.try_into() instead of NewSubscriber::try_from(form.0), because
+    // back converter automatically generates. But I prefer more explicit style.
+    let new_subscriber = match NewSubscriber::try_from(form.0) {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name
-    };
+
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+    fn try_from(form: FormData) -> Result<Self, String> {
+        let email = SubscriberEmail::parse(form.email)?;
+        let name = SubscriberName::parse(form.name)?;
+        Ok(Self { email, name })
+    }
+}
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
@@ -45,7 +52,7 @@ pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) ->
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
